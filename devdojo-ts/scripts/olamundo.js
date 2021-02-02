2800698b4c13d53c67916a692bf56a422fb2882a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-02 19:50:51 -0300

    Primeiro commit. Trabalhando com variaveis.

---
+++ devdojo-ts/scripts/olamundo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Enivaldo on 02/02/2021
+ */
+var p = document.createElement('p');
+var ola = "DevDojo Somos feras";
+p.textContent = ola;
+document.body.appendChild(p);
+//Tipos no typescript
+var num = 10;
+var bool = true;
+var str = "Um teste";
+var str1 = 'Um teste';
+var str2 = str1 + " to Enivaldo";
---
+++ devdojo-ts/scripts/olamundo.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Enivaldo on 02/02/2021
+ */
+var p = document.createElement('p');
+var ola: string = "DevDojo Somos feras";
+
+p.textContent = ola;
+document.body.appendChild(p);
+
+//Tipos no typescript
+var num : number = 10;
+var bool : boolean = true;
+var str: string = "Um teste";
+var str1: string = 'Um teste';
+var str2: string = `${str1} to Enivaldo`;
