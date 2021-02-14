4fffb2790a1ba5d28c5f08cc5650c2008304de7f
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-13 22:34:29 -0300

    Entendo a manipulação dos tipos e variaveis no typescript

--- devdojo-ts/scripts/olamundo.js
+++ devdojo-ts/scripts/olamundo.js
@@ -1,6 +1,7 @@
 /**
  * Created by Enivaldo on 02/02/2021
  */
+//Parte 01
 var p = document.createElement('p');
 var ola = "DevDojo Somos feras";
 p.textContent = ola;
@@ -8,6 +9,82 @@ document.body.appendChild(p);
 //Tipos no typescript
 var num = 10;
 var bool = true;
-var str = "Um teste";
-var str1 = 'Um teste';
-var str2 = str1 + " to Enivaldo";
+var str1 = "Um teste";
+var str2 = 'Um teste' +
+    ' Outro teste';
+var str3 = str1 + " to Enivaldo\nQueiroz";
+console.log(str3);
+//console.log(str2);
+//Função 
+function func() { }
+;
+if (bool != null) { }
+if (bool === undefined) { }
+if (bool) {
+    // let torna a varial local e var torna a varial global
+    var i = void 0;
+    for (i = 0; i < 3; i++) {
+        console.log(i);
+    }
+}
+//Não reconhece a variavel i pois não é global
+//console.log("Value of i is " + i);
+//console.log("Value of i is " + this.i);
+//Arry
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+//Tupla
+var tuple;
+//Dará erro
+//tuple = [77,"Enivaldo"];
+tuple = ["Enivaldo", 77];
+console.log(tuple);
+console.log(tuple[0].toLocaleUpperCase());
+console.log(tuple[1]);
+//Parte 02
+//Enumeração
+var Day;
+(function (Day) {
+    Day[Day["MONDAY"] = 0] = "MONDAY";
+    Day[Day["TUESDAY"] = 1] = "TUESDAY";
+    Day[Day["WEDNESDAY"] = 2] = "WEDNESDAY";
+})(Day || (Day = {}));
+var day = Day.MONDAY;
+console.log("day:" + day + " " + Day.TUESDAY + " " + Day.WEDNESDAY);
+console.log("day:" + Day[0] + " " + Day[1] + " " + Day[2]);
+//console.log(true === 1);
+/*? inserido o simbolo de interrogação a varialvel ficarar com o recebimento do valor facultativo */
+/*function add(v1:number, v2:number, v3?:number):  number {
+    if(v3 !== undefined)
+        return v1 + v2 + v3;
+    return v1 + v2;
+}
+
+console.log(`add ${add(1,2,3)}`);*/
+function add(v1, v2, v3) {
+    if (v3 !== undefined)
+        return v1 + v2 + v3;
+    return v1 + v2;
+}
+console.log("add " + add(1, 2));
+console.log("add(\"quem \u00E9 o\" , \"gatao\" ) ");
+var var1 = "Teste";
+console.log(var1);
+//let exemplo: number = undefined;
+//Parte 03
+var stringLength = var1.length;
+var stringUpperCase = var1.toLocaleUpperCase();
+console.log("Quantidade de caracteres: ", stringLength);
+console.log(stringUpperCase);
+function add2(v1) {
+    var v2 = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        v2[_i - 1] = arguments[_i];
+    }
+    var sum = 0;
+    for (var i = 0; i < v2.length; i++) {
+        sum += v2[i];
+    }
+    console.log(v1 + sum);
+}
+add2("Sum is: ", 1, 2, 4, 5);
--- devdojo-ts/scripts/olamundo.ts
+++ devdojo-ts/scripts/olamundo.ts
@@ -1,6 +1,8 @@
 /**
  * Created by Enivaldo on 02/02/2021
  */
+
+ //Parte 01
 var p = document.createElement('p');
 var ola: string = "DevDojo Somos feras";
 
@@ -8,8 +10,101 @@ p.textContent = ola;
 document.body.appendChild(p);
 
 //Tipos no typescript
-var num : number = 10;
-var bool : boolean = true;
-var str: string = "Um teste";
-var str1: string = 'Um teste';
-var str2: string = `${str1} to Enivaldo`;
+let num : number = 10;
+let bool : boolean = true;
+let str1: string = "Um teste";
+let str2: string = 'Um teste' + 
+                    ' Outro teste';
+var str3: string = `${str1} to Enivaldo
+Queiroz`;
+console.log(str3);
+//console.log(str2);
+
+//Função 
+function func():void{};
+
+if(bool != null){}
+if(bool === undefined){}
+
+if(bool){
+    // let torna a varial local e var torna a varial global
+    let i:number;
+    for (i= 0; i < 3; i++){
+        console.log(i);
+    }
+}
+//Não reconhece a variavel i pois não é global
+//console.log("Value of i is " + i);
+
+//console.log("Value of i is " + this.i);
+
+
+//Arry
+let list: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+//Tupla
+let tuple:[string,number];
+//Dará erro
+//tuple = [77,"Enivaldo"];
+tuple = ["Enivaldo", 77];
+console.log(tuple);
+console.log(tuple[0].toLocaleUpperCase());
+console.log(tuple[1]);
+
+//Parte 02
+
+//Enumeração
+
+enum Day{MONDAY, TUESDAY, WEDNESDAY}
+let day: Day = Day.MONDAY;
+console.log(`day:${day} ${Day.TUESDAY} ${Day.WEDNESDAY}`);
+
+console.log(`day:${Day[0]} ${Day[1]} ${Day[2]}`);
+
+
+//console.log(true === 1);
+/*? inserido o simbolo de interrogação a varialvel ficarar com o recebimento do valor facultativo */
+/*function add(v1:number, v2:number, v3?:number):  number {
+    if(v3 !== undefined)
+        return v1 + v2 + v3;
+    return v1 + v2;
+}
+
+console.log(`add ${add(1,2,3)}`);*/
+
+
+function add(v1:any, v2:any, v3?:any): void {
+    if(v3 !== undefined)
+        return v1 + v2 + v3;
+    return v1 + v2;
+}
+
+console.log(`add ${add(1,2)}`);
+console.log(`add("quem é o" , "gatao" ) `);
+
+let var1: any = "Teste";
+console.log(var1);
+
+//let exemplo: number = undefined;
+
+//Parte 03
+
+let stringLength: number = (<string> var1).length;
+let stringUpperCase: string = (var1 as string).toLocaleUpperCase();
+
+console.log("Quantidade de caracteres: ",stringLength);
+console.log(stringUpperCase);
+
+//
+function add2(v1:string, ...v2:number[]) :void {
+    let sum: number = 0;
+    for (let i = 0; i < v2.length; i++){
+        sum += v2[i];
+    }
+    console.log(v1 + sum);
+    
+}
+
+add2("Sum is: ", 1,2,4,5)
+
